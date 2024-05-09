2812180555d2e5cbe380fb1476eca6f9fd75a7f1
VickArmand <victormaina1962@gmail.com>
2024-05-09 16:25:44 +0300

    first commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+0-validate_utf8 module has a function validUTF8
+that determines if a given data set represents
+a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """"""
+    pass
