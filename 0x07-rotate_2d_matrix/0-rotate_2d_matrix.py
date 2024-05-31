2cdf8fb0b61bf4a2194723705bc71113eae3adac
VickArmand <victormaina1962@gmail.com>
2024-05-31 15:03:04 +0300

    corrections

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,10 +7,15 @@
 def rotate_2d_matrix(matrix):
     """performs in-place 90 degrees clockwise
     rotation of a matrix"""
-    size = len(matrix)
-    for row in range(size):
-        for column in range(size):
-            if row < size - 1 and column < size - 1 or row == size - 1:
-                swap_buffer = matrix[column][size - 1 - row]
-                matrix[column][size - 1 - row] = matrix[row][column]
-                matrix[row][column] = swap_buffer
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
--- 0x07-rotate_2d_matrix/main_0.py
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -4,17 +4,32 @@ Test 0x07 - Rotate 2D Matrix
 """
 rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
 
+
+def print_matrix(matrix):
+    print()
+    for r in matrix:
+        print(r)
+
+
 if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
 
     rotate_2d_matrix(matrix)
-    print(matrix)
+    print_matrix(matrix)
 
     matrix = [[1, 2, 3, 4],
               [5, 6, 7, 8],
               [9, 10, 11, 12],
               [13, 14, 15, 16]]
     rotate_2d_matrix(matrix)
-    print(matrix)
+    print_matrix(matrix)
+
+    matrix = [[1, 2, 3, 4, 5],
+              [6, 7, 8, 9, 10],
+              [11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20],
+              [21, 22, 23, 24, 25]]
+    rotate_2d_matrix(matrix)
+    print_matrix(matrix)
