4d1a84631f29aeb734402e5d29b3f3a8b8490e85
VickArmand <victormaina1962@gmail.com>
2024-06-06 05:18:11 +0300

    0. Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+0-making_change module contains
+a function called makeChange
+"""
+
+
+def makeChange(coins, total):
+    """
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    coins is a list of the values of the coins in your possession
+    The value of a coin will always be an integer greater than 0
+    You can assume you have an infinite number of each denomination
+    of coin in the list
+    """
+    if total <= 0:
+        return 0
+    if sum(coins) > total:
+        return -1
+    total -= sum(coins)
+    min_coins = len(coins)
+    while total > 0 and len(coins) > 0:
+        max_denomination = max(coins)
+        if total - max_denomination > 0:
+            total -= max_denomination
+            min_coins += 1
+        else:
+            coins.remove(max_denomination)
+    return min_coins
