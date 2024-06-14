ce75b42fa7736b5d7ac3db0dfad5492a3780fd39
VickArmand <victormaina1962@gmail.com>
2024-06-14 11:39:55 +0300

    corrections

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,9 +4,29 @@
 
 def perimeter_cell(grid, i, j):
     """Determines the perimeter of one cell"""
-    size = len(grid)
+    rows = len(grid)
+    columns = len(grid[i])
     perimeter = 4
-    if j == 0 and i != 0:
+    if i == 0 and j == 0:
+        if grid[i + 1][j] == 1:
+            perimeter -= 1
+        if grid[i][j + 1] == 1:
+            perimeter -= 1
+    elif i == rows - 1 and j != columns - 1:
+        if grid[i - 1][j] == 1:
+            perimeter -= 1
+        if grid[i][j - 1] == 1:
+            perimeter -= 1
+        if grid[i][j + 1] == 1:
+            perimeter -= 1
+    elif j == columns - 1 and i != rows - 1:
+        if grid[i - 1][j] == 1:
+            perimeter -= 1
+        if grid[i][j - 1] == 1:
+            perimeter -= 1
+        if grid[i + 1][j] == 1:
+            perimeter -= 1
+    elif j == 0 and i != 0:
         if grid[i + 1][j] == 1:
             perimeter -= 1
         if grid[i][j + 1] == 1:
@@ -20,26 +40,7 @@ def perimeter_cell(grid, i, j):
             perimeter -= 1
         if grid[i][j - 1] == 1:
             perimeter -= 1
-    elif i == 0 and j == 0:
-        if grid[i + 1][j] == 1:
-            perimeter -= 1
-        if grid[i][j + 1] == 1:
-            perimeter -= 1
-    elif i == size - 1 and j != size - 1:
-        if grid[i - 1][j] == 1:
-            perimeter -= 1
-        if grid[i][j - 1] == 1:
-            perimeter -= 1
-        if grid[i][j + 1] == 1:
-            perimeter -= 1
-    elif j == size - 1 and i != size - 1:
-        if grid[i - 1][j] == 1:
-            perimeter -= 1
-        if grid[i][j - 1] == 1:
-            perimeter -= 1
-        if grid[i + 1][j] == 1:
-            perimeter -= 1
-    elif j == size - 1 and i == size - 1:
+    elif j == columns - 1 and i == rows - 1:
         if grid[i - 1][j] == 1:
             perimeter -= 1
         if grid[i][j - 1] == 1:
@@ -71,9 +72,10 @@ def island_perimeter(grid):
     (water inside that isn’t connected to the water surrounding the island).
     """
     grid_perimeter = 0
-    size = len(grid)
-    for i in range(size):
-        for j in range(size):
+    rows = len(grid)
+    for i in range(rows):
+        columns = len(grid[i])
+        for j in range(columns):
             if grid[i][j] == 1:
                 cell_perimeter = perimeter_cell(grid, i, j)
                 grid_perimeter += cell_perimeter
