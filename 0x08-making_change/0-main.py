aa5603d8338b5d7f2ace6311466c464597189b72
VickArmand <victormaina1962@gmail.com>
2024-06-17 18:40:00 +0300

    fianl tests

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -9,22 +9,22 @@ makeChange = __import__('0-making_change').makeChange
 print(makeChange([1, 2, 25], 37))
 print(makeChange([1, 2, 10], 11))
 print(makeChange([3, 6, 9], 1278652))
-# print(makeChange([], 37))
-# print(makeChange([1256, 54, 48, 16, 102], 1453))
-# coins = []
-# for i in range(1, 1000, 7):
-#     coins.append(i)
-# print(makeChange(coins, 16576))
-# start = time.time()
+print(makeChange([], 37))
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+coins = []
+for i in range(1, 1000, 7):
+    coins.append(i)
+print(makeChange(coins, 16576))
+start = time.time()
 
-# for i in range(10):
-#     makeChange([2, 4, 6, 10], 1278653)
+for i in range(10):
+    makeChange([2, 4, 6, 10], 1278653)
 
-# end = time.time()
+end = time.time()
 
-# avg = (end - start) / 10
+avg = (end - start) / 10
 
-# if avg > 3:
-#     print("Runtime too long")
-# else:
-#     print("OK")
+if avg > 3:
+    print("Runtime too long")
+else:
+    print("OK")
